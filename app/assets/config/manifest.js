001e82bf92667662b5ff4843f7a8aa9ff780dd7d
ayumi0511 <ayumi.tokoeda@gmail.com>
2023-10-24 17:05:41 +0900

    修正 #1(link_tree削除）

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,1 @@
-//= link_tree ../../../app/views/pages/dist/assets
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../app/views/pages/dist
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :new, :create]
   get 'user', to: 'users#index'
   get '/daily_index', to: 'daily#index'
+  root 'daily#index'
   resources :subscriptions
   resources :orders
   resources :daily
